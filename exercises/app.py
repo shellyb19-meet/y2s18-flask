b8280fd3f4e7a14da6c4a0d39dc9d53f7deb3a2c
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 16:38:36 +0300

    yasssss

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    players=["messi", "ronaldo", "naimar"]
+    return render_template("index.html", 
+    players=players,
+    like_same_sport=True)
 def home_page():
-    pass
+    return "ballet"
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
